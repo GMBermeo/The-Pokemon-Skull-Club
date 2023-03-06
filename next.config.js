3cbf19cd22aa320afe45946173e37ac7b29a541d
Guilherme Bermêo <guilherme.bermeo@agu.gov.br>
2023-03-06 11:45:24 -0300

    🚀 Initial Commit (Next.js13 + MUI boilerplate model)

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ["cards.scryfall.io"],
-  },
-  i18n: {
-    locales: ["en-US"],
-    defaultLocale: "en-US",
-
-    domains: [
-      {
-        // Note: subdomains must be included in the domain value to be matched
-        // e.g. www.example.com should be used if that is the expected hostname
-        domain: "dogs-of-mtg.bermeo.dev",
-        defaultLocale: "en-US",
-      },
-    ],
-  },
+  experimental: { appDir: true },
 };
 
 module.exports = nextConfig;
