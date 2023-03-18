5b315c600f7fb010f64bf5f35aa2b74d3a63cf6d
Guilherme Bermêo <gm@bermeo.dev>
2023-03-18 18:34:56 -0300

    🐛 Removed broken import

--- pages/ex.tsx
+++ pages/ex.tsx
@@ -5,7 +5,6 @@ import Masonry from "@mui/lab/Masonry";
 
 import { styled } from "@mui/material/styles";
 import { Stack, Typography, Paper } from "@mui/material";
-import { loadCards } from "../lib/loadCardsOriginal";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
