e578e89f99b85b63932731c65450b5aed160081d
Guilherme Bermêo <guilherme.bermeo@agu.gov.br>
2025-02-03 17:31:35 -0300

    🐛 Fix link in BotaoEbay to word as SSR

--- src/app/components/BotaoEbay.tsx
+++ src/app/components/BotaoEbay.tsx
@@ -1,15 +1,21 @@
+"use server";
+import { JSX } from "react";
 import { PokemonTCG } from "pokemon-tcg-sdk-typescript";
 
 interface BotaoEbayProps {
   card: PokemonTCG.Card;
 }
 
-export const BotaoEbay = (props: Readonly<BotaoEbayProps>) => {
+export const BotaoEbay = async (
+  props: Readonly<BotaoEbayProps>
+): Promise<JSX.Element> => {
+  const ebayUrl = `https://www.ebay.com/sch/i.html?_nkw=${props.card.name}+${props.card.id}&mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339099221&customid=&toolid=10001&mkevt=1`;
+
   return (
     <a
       target="_blank"
       className="bg-blue-600 hover:bg-blue-500 text-white px-8 py-2 rounded-md"
-      href={`https://www.ebay.com/sch/i.html?_nkw=${props.card.name}+${props.card.id}&mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5339099221&customid=&toolid=10001&mkevt=1`}
+      href={ebayUrl}
     >
       Buy
     </a>
