1570c1c1124da7b625917c43dc1db4895dd4cb2a
Guilherme Bermêo <guilherme.bermeo@agu.gov.br>
2025-02-03 17:54:46 -0300

    🚨 Fix ESLint using Next structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,12 +29,13 @@ export default async function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
-        <script
+        <Script
+          id="epn-config"
           dangerouslySetInnerHTML={{
             __html: `window._epn = {campaign: 5339099221};`,
           }}
         />
-        <script src="https://epnt.ebay.com/static/epn-smart-tools.js" />
+        <Script src="https://epnt.ebay.com/static/epn-smart-tools.js" />
       </head>
       <body className={inter.className}>
         {children}
